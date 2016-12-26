846e4379f824d3e7d432ce77ae20d4fa524b85f3
arbuz93 <93.endo@gmail.com>
2016-12-26 18:07:03 +0100

    Dopracowanie skryptu reset.sh v2.0

--- reset.sh
+++ reset.sh
@@ -10,16 +10,16 @@ rm -r $(dirname $0)/statement/migrations/*
 rm -r $(dirname $0)/translator/migrations/*
 rm -r $(dirname $0)/user/migrations/*
 
-python $(dirname $0)/manage.py makemigrations main
-python $(dirname $0)/manage.py makemigrations product
-python $(dirname $0)/manage.py makemigrations root
-python $(dirname $0)/manage.py makemigrations sender
-python $(dirname $0)/manage.py makemigrations session
-python $(dirname $0)/manage.py makemigrations statement
-python $(dirname $0)/manage.py makemigrations translator
-python $(dirname $0)/manage.py makemigrations user
+python3 $(dirname $0)/manage.py makemigrations main
+python3 $(dirname $0)/manage.py makemigrations product
+python3 $(dirname $0)/manage.py makemigrations root
+python3 $(dirname $0)/manage.py makemigrations sender
+python3 $(dirname $0)/manage.py makemigrations session
+python3 $(dirname $0)/manage.py makemigrations statement
+python3 $(dirname $0)/manage.py makemigrations translator
+python3 $(dirname $0)/manage.py makemigrations user
 
-python $(dirname $0)/manage.py migrate
+python3 $(dirname $0)/manage.py migrate
 chmod 664 $(dirname $0)/db.sqlite3
 
 GET http://127.0.0.1:8000/setting/load_languages/ > /dev/null
