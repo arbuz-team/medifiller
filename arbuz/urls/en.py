c0e556c060ac62234501739ed63be1be2c21a666
arbuz93 <93.endo@gmail.com>
2016-12-28 14:55:36 +0100

    Poprawka przekierowań urls.

--- arbuz/urls/en.py
+++ arbuz/urls/en.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^user/', include('user.urls')),
-    url(r'^root/', include('root.urls')),
-    url(r'^statement/', include('statement.urls')),
-    url(r'^product/', include('product.urls')),
-    url(r'^setting/', include('setting.urls')),
-    url(r'', include('main.urls')),
+    url(r'^user/', include('user.urls.en')),
+    url(r'^root/', include('root.urls.en')),
+    url(r'^statement/', include('statement.urls.en')),
+    url(r'^product/', include('product.urls.en')),
+    url(r'^setting/', include('setting.urls.en')),
+    url(r'', include('main.urls.en')),
 ]
--- arbuz/urls/pl.py
+++ arbuz/urls/pl.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^użytkownik/', include('user.urls')),
-    url(r'^administrator/', include('root.urls')),
-    url(r'^komunikat/', include('statement.urls')),
-    url(r'^produkt/', include('product.urls')),
-    url(r'^ustawienia/', include('setting.urls')),
-    url(r'', include('main.urls')),
+    url(r'^użytkownik/', include('user.urls.pl')),
+    url(r'^administrator/', include('root.urls.pl')),
+    url(r'^komunikat/', include('statement.urls.pl')),
+    url(r'^produkt/', include('product.urls.pl')),
+    url(r'^ustawienia/', include('setting.urls.pl')),
+    url(r'', include('main.urls.pl')),
 ]
