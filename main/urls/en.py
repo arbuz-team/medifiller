dd796404df95630d5e4f1beb7d3a5af7302a6158
arbuz93 <93.endo@gmail.com>
2017-01-04 21:37:55 +0100

    Append /products/ url.

--- main/urls/en.py
+++ main/urls/en.py
@@ -4,7 +4,8 @@ from main import views
 urlpatterns = [
     url(r'^$', views.Contains_Start.Launch, name='main.start'),
     url(r'^about_us/$', views.Contains_About_Us.Launch, name='main.about_us'),
-    url(r'^product/$', views.Contains_Products.Launch, name='main.products'),
+    url(r'^product/$', views.Contains_Products.Launch, name='main.product'),
+    url(r'^products/$', views.Contains_Products.Launch, name='main.products'),
     url(r'^contact_us/$', views.Contains_Contact_Us.Launch, name='main.contact_us'),
     url(r'^edit/$', views.Contains_Edit.Launch, name='main.edit'),
 ]
