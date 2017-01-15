f4d4206ba4d8e8fb9d768fd19448265b489a61cb
arbuz93 <93.endo@gmail.com>
2017-01-16 00:44:09 +0100

    UP

--- main/urls/pl.py
+++ main/urls/pl.py
@@ -3,8 +3,8 @@ from main import views
 
 urlpatterns = [
     url(r'^$', views.Contains_Start.Launch),
-    url(r'^o_nas/$', views.Contains_About_Us.Launch),
+    url(r'^o_nas/$', views.Contains_About.Launch),
     url(r'^produkty/$', views.Contains_Products.Launch),
-    url(r'^kontakt/$', views.Contains_Contact_Us.Launch),
+    url(r'^kontakt/$', views.Contains_Contact.Launch),
     url(r'^edycja/$', views.Contains_Edit.Launch),
 ]
