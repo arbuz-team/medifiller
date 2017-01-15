3ec11b12ede9a19c8eb0e8af60362f4764338e82
arbuz93 <93.endo@gmail.com>
2017-01-16 00:41:01 +0100

    The little fix of last commit.

--- arbuz/urls/pl.py
+++ arbuz/urls/pl.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^uzytkownik/', include('user.urls.en'), name='user'),
-    url(r'^administrator/', include('root.urls.en'), name='root'),
-    url(r'^komunikat/', include('statement.urls.en'), name='statement'),
-    url(r'^produkt/', include('product.urls.en'), name='product'),
-    url(r'^ustawienia/', include('setting.urls.en'), name='setting'),
-    url(r'', include('main.urls.en'), name='main'),
+    url(r'^uzytkownik/', include('user.urls.pl'), name='user'),
+    url(r'^administrator/', include('root.urls.pl'), name='root'),
+    url(r'^komunikat/', include('statement.urls.pl'), name='statement'),
+    url(r'^produkt/', include('product.urls.pl'), name='product'),
+    url(r'^ustawienia/', include('setting.urls.pl'), name='setting'),
+    url(r'', include('main.urls.pl'), name='main'),
 ]
--- user/urls/pl.py
+++ user/urls/pl.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from user import views
 
 urlpatterns = [
-    url(r'^zaloguj/$', views.Login.Launch, name='user.login'),
-    url(r'^zarejestruj/$', views.Register.Launch, name='user.register'),
-    url(r'^wyloguj/$', views.Logout.Launch, name='user.logout'),
+    url(r'^$', views.Start_App.Launch, name='user.start'),
+    url(r'^zaloguj/$', views.Sign_In.Launch, name='user.sign_in'),
+    url(r'^zarejestruj/$', views.Sign_Up.Launch, name='user.sign_up'),
+    url(r'^wyloguj/$', views.Sign_Out.Launch, name='user.sign_out'),
     url(r'^konto/$', views.Account.Launch, name='user.account'),
     url(r'^aktywuj/(?P<key>[a-z0-9]{40})/$',
         views.Approved_Register.Update_User_Status, name='user.approved'),
