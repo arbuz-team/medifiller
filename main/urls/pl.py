a47208cd3ade758c193db1b7d0f1965d2ab40bb2
arbuz93 <93.endo@gmail.com>
2017-01-16 01:02:14 +0100

    UP

--- main/urls/pl.py
+++ main/urls/pl.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from main import views
 
 urlpatterns = [
-    url(r'^$', views.Contains_Start.Launch),
-    url(r'^o_nas/$', views.Contains_About.Launch),
-    url(r'^produkty/$', views.Contains_Products.Launch),
-    url(r'^kontakt/$', views.Contains_Contact.Launch),
-    url(r'^edycja/$', views.Contains_Edit.Launch),
+    url(r'^$', views.Contains_Start.Launch, name="main.start"),
+    url(r'^o_nas/$', views.Contains_About.Launch, name="main.about"),
+    url(r'^produkty/$', views.Contains_Products.Launch, name="main.products"),
+    url(r'^kontakt/$', views.Contains_Contact.Launch, name="main.contact"),
+    url(r'^edycja/$', views.Contains_Edit.Launch, name="main.edit"),
 ]
