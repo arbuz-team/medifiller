d45a773bafee9f22f84ad5f9ec71981dbc2d8166
arbuz93 <93.endo@gmail.com>
2017-02-03 01:45:27 +0100

    Small change

--- payment/views.py
+++ payment/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from paypal.standard.forms import PayPalPaymentsForm
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received
+from arbuz.settings import PAYPAL_RECEIVER_EMAIL
 
 
 class Payment(Dynamic_Event_Menager):
@@ -31,7 +32,7 @@ class Payment(Dynamic_Event_Menager):
 
         # What you want the button to do.
         paypal_dict = {
-            'business': '93.endo-facilitator@gmail.com',
+            'business': PAYPAL_RECEIVER_EMAIL,
             'amount': self.content['total_price'],
             'item_name': 'sungate',
             #'invoice': 'unique-invoice-id',  # id faktury
@@ -54,7 +55,7 @@ class Payment(Dynamic_Event_Menager):
             # Check that the receiver email is the same we previously
             # set on the business field request. (The user could tamper
             # with those fields on payment form before send it to PayPal)
-            if ipn.receiver_email != '93.endo-facilitator@gmail.com':
+            if ipn.receiver_email != PAYPAL_RECEIVER_EMAIL:
                 return
 
             # ALSO: for the same reason, you need to check the amount
@@ -65,8 +66,9 @@ class Payment(Dynamic_Event_Menager):
                 user = User.objects.get(unique=ipn.custom)
                 cart = Cart.objects.filter(user=user)
 
-                if ipn.amount != Payment.Get_Total_Price(user):
-                    return
+                print(ipn)
+                #if ipn.amount != Payment.Get_Total_Price(user):
+                #    return
 
                 for in_cart in cart:
                     in_cart.approved = True
