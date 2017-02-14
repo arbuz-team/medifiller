1ac06e2207cf3e00ffb3dedba31e400d92796051
arbuz93 <93.endo@gmail.com>
2017-02-14 03:00:21 +0100

    Fixed URL files of PL language and created 'get_url' tag.

--- arbuz/templatetags/tags.py
+++ arbuz/templatetags/tags.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django import template
 register = template.Library()
 
@@ -10,7 +11,7 @@ def to_money(value):
     return format(value / 100, '.2f')
 
 @register.filter('get_price')
-def get_price(product, currency):
+def get_price(product, request):
 
     prices = {
         'EUR': product.price_eur,
@@ -19,5 +20,18 @@ def get_price(product, currency):
         'USD': product.price_usd
     }
 
+    currency = request.session['translator_currency']
     price = str(to_money(prices[currency]))
     return '{0} {1}'.format(price, currency)
+
+@register.filter('get_url')
+def get_url(request, name):
+
+    urls = {
+        'EN': reverse(name, urlconf='arbuz.urls.en'),
+        'PL': reverse(name, urlconf='arbuz.urls.pl'),
+        # 'DE': reverse(name, urlconf='arbuz.urls.de'),
+    }
+
+    language = request.session['translator_language']
+    return urls[language]
--- arbuz/urls/pl.py
+++ arbuz/urls/pl.py
@@ -1,16 +1,16 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^uzytkownik/', include('user.urls.en'), name='user'),
-    url(r'^administrator/', include('root.urls.en'), name='root'),
-    url(r'^komunikat/', include('statement.urls.en'), name='statement'),
-    url(r'^produkt/', include('product.urls.en'), name='product'),
-    url(r'^ustawienia/', include('setting.urls.en'), name='setting'),
-    url(r'^wyszukiwarka/', include('searcher.urls.en'), name='searcher'),
-    url(r'^koszyk/', include('cart.urls.en'), name='cart'),
-    url(r'^nawigacja/', include('navigation.urls.en'), name='navigation'),
-    url(r'^platnosci/', include('payment.urls.en'), name='payment'),
+    url(r'^uzytkownik/', include('user.urls.pl'), name='user'),
+    url(r'^administrator/', include('root.urls.pl'), name='root'),
+    url(r'^komunikat/', include('statement.urls.pl'), name='statement'),
+    url(r'^produkt/', include('product.urls.pl'), name='product'),
+    url(r'^ustawienia/', include('setting.urls.pl'), name='setting'),
+    url(r'^wyszukiwarka/', include('searcher.urls.pl'), name='searcher'),
+    url(r'^koszyk/', include('cart.urls.pl'), name='cart'),
+    url(r'^nawigacja/', include('navigation.urls.pl'), name='navigation'),
+    url(r'^platnosci/', include('payment.urls.pl'), name='payment'),
     url(r'^paypal/', include('paypal.standard.ipn.urls'), name='paypal-ipn'),
-    url(r'', include('main.urls.en'), name='main'),
+    url(r'', include('main.urls.pl'), name='main'),
 ]
 
