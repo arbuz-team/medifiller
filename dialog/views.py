3283b478d010c8a84186fc2d7176df0eee4fd2ec
arbuz93 <93.endo@gmail.com>
2017-02-25 02:04:46 +0100

    Fixed dialogs.

--- dialog/views.py
+++ dialog/views.py
@@ -5,12 +5,33 @@ from inspect import getmembers, ismethod
 
 class Dialog(Dynamic_Base):
 
+    def Render_Dialog(self, file_name, form_name = '',
+                      authorization=False, only_root=False):
+
+        if not authorization and not only_root:
+            return self.Render_HTML(file_name, form_name)
+
+        if authorization:
+            if self.request.session['user_login']:
+                return self.Render_HTML(file_name, form_name)
+
+        if only_root:
+            if self.request.session['root_login']:
+                return self.Render_HTML(file_name, form_name)
+
+        return self.Unauthorized_Access()
+
     def Get_Dialog_Name(self):
         return self.request.POST['dialog_name']
 
     def Apply_Message(self):
         self.content['title'] = Text(self.request, 8)
         return self.Render_HTML('dialog/apply.html')
+
+    def Unauthorized_Access(self):
+        self.content['title'] = Text(self.request, 69)
+        self.content['text'] = Text(self.request, 70)
+        return self.Render_HTML('dialog/alert.html')
 
     def Manage(self):
 
@@ -34,12 +55,12 @@ class Dialog(Dynamic_Base):
 class Dialog_Alert(Dialog):
 
     def Manage_Language(self):
-        return self.Render_HTML('dialog/language.html')
+        return self.Render_Dialog('dialog/language.html')
 
     def Manage_Product_Recommended(self):
         self.content['title'] = Text(self.request, 9)
         self.content['text'] = Text(self.request, 10)
-        return self.Render_HTML('dialog/alert.html')
+        return self.Render_Dialog('dialog/alert.html', only_root=True)
 
     def __init__(self, request):
         super(Dialog_Alert, self).__init__(request)
@@ -65,7 +86,8 @@ class Dialog_Prompt(Dialog):
         self.content['form'] = Form_Brand(self.request,
             self.Get_POST(), initial={'exists': initial})
 
-        return self.Render_HTML('dialog/prompt.html', 'brand')
+        return self.Render_Dialog('dialog/prompt.html',
+                                  'brand', only_root=True)
 
     def Manage_Purpose(self):
         initial = self.Get_Session_Variable()
@@ -75,35 +97,40 @@ class Dialog_Prompt(Dialog):
         self.content['form'] = Form_Purpose(self.request,
             self.Get_POST(), initial={'exists': initial})
 
-        return self.Render_HTML('dialog/prompt.html', 'purpose')
+        return self.Render_Dialog('dialog/prompt.html',
+                                  'purpose', only_root=True)
 
     def Manage_Details_EN(self):
         self.content['title'] = Text(self.request, 3)
         self.content['form'] = Form_Details_EN(self.request,
             self.Get_POST(), instance=self.Get_Session_Variable())
 
-        return self.Render_HTML('dialog/prompt.html', 'details_en')
+        return self.Render_Dialog('dialog/prompt.html',
+                                  'details_en', only_root=True)
 
     def Manage_Details_PL(self):
         self.content['title'] = Text(self.request, 4)
         self.content['form'] = Form_Details_PL(self.request,
             self.Get_POST(), instance=self.Get_Session_Variable())
 
-        return self.Render_HTML('dialog/prompt.html', 'details_pl')
+        return self.Render_Dialog('dialog/prompt.html',
+                                  'details_pl', only_root=True)
 
     def Manage_Details_DE(self):
         self.content['title'] = Text(self.request, 5)
         self.content['form'] = Form_Details_EN(self.request,
             self.Get_POST(), instance=self.Get_Session_Variable())
 
-        return self.Render_HTML('dialog/prompt.html', 'details_de')
+        return self.Render_Dialog('dialog/prompt.html',
+                                  'details_de', only_root=True)
 
     def Manage_Where_Display(self):
         self.content['title'] = Text(self.request, 6)
         self.content['form'] = Form_Where_Display(self.request,
             self.Get_POST(), instance=self.Get_Session_Variable())
 
-        return self.Render_HTML('dialog/prompt.html', 'where_display')
+        return self.Render_Dialog('dialog/prompt.html',
+                                  'where_display', only_root=True)
 
     def Manage_Image(self):
         initial = self.Get_Session_Variable()
@@ -114,7 +141,8 @@ class Dialog_Prompt(Dialog):
         self.content['form'] = Form_Image(self.request,
             self.Get_POST(), initial={'url': url})
 
-        return self.Render_HTML('dialog/prompt.html', 'image')
+        return self.Render_Dialog('dialog/prompt.html',
+                                  'image', only_root=True)
 
 
     def Get_POST(self):
